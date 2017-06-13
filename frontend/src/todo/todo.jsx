d317771e0a407e33244ed46e4bb6c4f5ccdf1fe5
Joelton Guerreiro <joeltonguerreiro@gmail.com>
2017-06-13 11:12:17 -0300

    finalizando app todos

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,84 +1,13 @@
-import React, { Component} from 'react'
-import Axios from 'axios'
+import React from 'react'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
-const URL = 'http://localhost:3003/api/todos'
-
-export default class Todo extends Component {
-
-    constructor(props){
-        super(props)
-
-        this.state = { description: '', list: [] }
-        this.handleAdd = this.handleAdd.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-        this.handleDone = this.handleDone.bind(this)
-        this.handleRemove = this.handleRemove.bind(this)
-        this.handlePending = this.handlePending.bind(this)
-        this.handleSearch = this.handleSearch.bind(this)
-        this.handleClear = this.handleClear.bind(this)
-        this.refresh()
-    }
-
-    refresh(description = '') {
-        const search = description ? `&description__regex=/${description}/` : ''
-        Axios.get(`${URL}?sort=-createdAt${search}`)
-            .then(resp => this.setState({ ...this.state, description, list: resp.data }))
-    }
-
-    handleAdd() {
-        const description = this.state.description
-        Axios.post(URL, { description })
-            .then(resp => this.refresh())
-    }
-
-    handleChange(e) {
-        this.setState({ ...this.state, description: e.target.value })
-    }
-
-    handleRemove(todo) {
-        Axios.delete(`${URL}/${todo._id}`)
-            .then(resp => this.refresh(this.state.description))
-    }
-
-    handleDone(todo) {
-        todo.done = true
-        Axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
-            .then(resp => this.refresh(this.state.description))
-    }
-
-    handlePending(todo) {
-        Axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
-            .then(resp => this.refresh(this.state.description))
-    }
-
-    handleSearch() {
-        this.refresh(this.state.description)
-    }
-
-    handleClear() {
-        this.refresh()
-    }
-
-    render() {
-        return (
-            <div>  
-                <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm 
-                    description={this.state.description}
-                    handleAdd={this.handleAdd}
-                    handleChange={this.handleChange}
-                    handleSearch={this.handleSearch}
-                    handleClear={this.handleClear} />
-                <TodoList 
-                    list={this.state.list}
-                    handleDone={this.handleDone}
-                    handlePending={this.handlePending}
-                    handleRemove={this.handleRemove} />
-            </div>
-        )
-    }
-}
+export default props => (
+    <div>  
+        <PageHeader name='Tarefas' small='Cadastro' />
+        <TodoForm />
+        <TodoList />
+    </div>
+)
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription, search, add } from './todoActions'
+import { changeDescription, search, add, clear } from './todoActions'
 
 class TodoForm extends Component {
     constructor (props) {
@@ -19,11 +19,11 @@ class TodoForm extends Component {
     }
 
     keyHandler (e) {
-        const { add, search, description } = this.props
+        const { add, search, description, clear } = this.props
         if(e.key === 'Enter') {
             e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
-            props.handleClear()
+            clear()
         }
     }
 
@@ -42,9 +42,9 @@ class TodoForm extends Component {
                     <IconButton style='primary' icon='plus' 
                         onClick={() => add(description)} hide={false}/>
                     <IconButton style='info' icon='search' 
-                        onClick={() => search() } hide={false}/>
+                        onClick={search } hide={false}/>
                     <IconButton style='default' icon='times' 
-                        onClick={this.props.handleClear} hide={false}/>
+                        onClick={this.props.clear} hide={false}/>
                 </Grid>
             </div>
         )
@@ -53,9 +53,5 @@ class TodoForm extends Component {
 
 const mapStateToProps = state => ({description: state.todo.description})
 const mapDispacthToProps = dispatch => 
-    bindActionCreators({ 
-        changeDescription,
-        search,
-        add
-    }, dispatch)
+    bindActionCreators({ changeDescription, search, add, clear }, dispatch)
 export default connect(mapStateToProps, mapDispacthToProps)(TodoForm)
